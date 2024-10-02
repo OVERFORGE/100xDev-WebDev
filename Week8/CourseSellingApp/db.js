b1234124ac50c5bd9ace0eb4f988105b62fc7110
DAKSH KAUSHAL <129703289+OVERFORGE@users.noreply.github.com>
2024-10-02 17:50:20 +0530

    Add files via upload

---
+++ Week8/CourseSellingApp/db.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const adminSchema = new schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const courseSchema = new schema({
+  title: String,
+  description: String,
+  price: String,
+  imageUrl: String,
+  creatorId: ObjectId,
+});
+
+const purchaseSchema = new schema({
+  userId: ObjectId,
+  courseId: ObjectId,
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+  userModel,
+  adminModel,
+  courseModel,
+  purchaseModel,
+};
